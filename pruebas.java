4944d747c09bb9a1b8e5bec51ea8ce40c642f081
BrayanCo <brayancol36@gmail.com>
2021-03-01 18:54:03 -0500

    Cuarto Cambio calculamos edad

--- pruebas.java
+++ pruebas.java
@@ -14,11 +14,25 @@ public class holaComoEstas {
 		return PI * Math.pow(radio, 2);
 	}
 
+	public int determineAge ( int bornDay, int bornMonth, int bornYear) {
+		LocalDate currentDate = LocalDate.now();
+		int currentYear = currentDate.getYear();
+		int currentMonth = currentDate.getMonthValue();
+		int currentDay = currentDate.getDayOfMonth();
+		int age = currentYear - bornYear;
+		if( bornMonth > currentMonth) {
+			--age;
+		}else if( bornMonth == currentMonth && bornDay > currentDay ) {
+			--age;
+		}
+		return age;
+		}
+
  public static void main(String[] args) {
 	System.out.println(new holaComoEstas().areaCirculo(10));
 	System.out.println("ESTE SI POR FAVOR :'V");
 	System.out.println(buenas);
-	System.out.println();
+	System.out.println(new holaComoEstas.determineAge(12,8,2001));
 }
 
 }
