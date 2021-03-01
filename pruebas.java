e0f0a80cbd63e5d123434b152b67ae18a9aa44f9
unknown <leon.amaya270@gmail.com>
2021-03-01 18:38:29 -0500

    Agrego metodo area de circulo

--- pruebas.java
+++ pruebas.java
@@ -1,5 +1,7 @@
 public class holaComoEstas {
 
+ 	public static final double PI = 3.14159;
+
 
  	private int a=300;
 
@@ -8,10 +10,15 @@ public class holaComoEstas {
 		return a;
 	}
 
+	public double areaCirculo(int radio){
+		return PI * Math.pow(radio, 2);
+	}
+
  public static void main(String[] args) {
-	
+	System.out.println(new holaComoEstas().areaCirculo(10));
 	System.out.println("ESTE SI POR FAVOR :'V");
 	System.out.println(buenas);
+	System.out.println();
 }
 
 }
