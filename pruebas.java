5d415198b56a8cb75d9f4e31cf859c10a680613d
BrayanCo <brayancol36@gmail.com>
2021-03-01 17:37:31 -0500

    Segundo Cambio :3

--- pruebas.java
+++ pruebas.java
@@ -1,9 +1,17 @@
 public class holaComoEstas {
 
 
+ 	private int a=300;
+
+	public int buenas (){
+
+		return a;
+	}
+
  public static void main(String[] args) {
 	
 	System.out.println("ESTE SI POR FAVOR :'V");
+	System.out.println(buenas);
 }
 
 }
